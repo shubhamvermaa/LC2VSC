6258cf23630811196ac30e2cc1f1791ca831b132
shubhamvermaa <vshubham0710@gmail.com>
2024-08-29 02:11:09 +0530

    fix dynamic component

--- app/Contest/page.js
+++ app/Contest/page.js
@@ -1,16 +1,28 @@
-
+import { useEffect, useState } from 'react';
 import CodeMirrorComponent from '../components/CodeMirrorComponent';
+
 export default function Contest() {
-    return (
-        <>
-            <h1>Old Contest Layout Parsing.</h1>
-            <CodeMirrorComponent parserType = "contest"/>
-            <h2>Usage:</h2>
-            <h3>
-                • Open the problem --&gt; Right-Click on the page  --&gt; Click View Page Source --&gt; Copy the content of the page source.<br></br>
-                • Paste the copied HTML in the above editor.<br></br>
-                • Click Parse to get parsed question, copy it to your clipboard and paste it in your CodeEditor.
-            </h3>
-        </>
-    )
+  const [isClient, setIsClient] = useState(false);
+
+  // Check if the component is running on the client side
+  useEffect(() => {
+    if (typeof navigator !== 'undefined') {
+      setIsClient(true);
+    }
+  }, []);
+
+  return (
+    <>
+      <h1>Old Contest Layout Parsing.</h1>
+
+      {isClient && <CodeMirrorComponent parserType="contest" />}
+
+      <h2>Usage:</h2>
+      <h3>
+        • Open the problem --&gt; Right-Click on the page  --&gt; Click View Page Source --&gt; Copy the content of the page source.<br></br>
+        • Paste the copied HTML in the above editor.<br></br>
+        • Click Parse to get parsed question, copy it to your clipboard and paste it in your CodeEditor.
+      </h3>
+    </>
+  );
 }
--- app/DynamicLayout/page.js
+++ app/DynamicLayout/page.js
@@ -1,16 +1,29 @@
-
+"use client"
+import { useEffect, useState } from 'react';
 import CodeMirrorComponent from '../components/CodeMirrorComponent';
+
 export default function Question() {
-    return (
-        <>
-            <h1>Dynamic Layout Parsing.</h1>
-            <CodeMirrorComponent parserType = "dynamic"/>
-            <h2>Usage:</h2>
-            <h3>
-                • Press Ctr+Shift+I --&gt; go to "element" tab --&gt; Right-Click on HTML --&gt; Copy-OuterHTML.<br></br>
-                • Paste the copied HTML in the above editor, wait for few seconds as the dynamic layout HTML is very large in size.<br></br>
-                • Click Parse to get parsed question, copy it to your clipboard and paste it in your CodeEditor.
-            </h3>
-        </>
-    )
+  const [isClient, setIsClient] = useState(false);
+
+  // Check if the component is running on the client side
+  useEffect(() => {
+    if (typeof navigator !== 'undefined') {
+      setIsClient(true);
+    }
+  }, []);
+
+  return (
+    <>
+      <h1>Dynamic Layout Parsing.</h1>
+
+      {isClient && <CodeMirrorComponent parserType="dynamic" />}
+
+      <h2>Usage:</h2>
+      <h3>
+        • Press Ctrl+Shift+I --&gt; go to "element" tab --&gt; Right-Click on HTML --&gt; Copy-OuterHTML.<br />
+        • Paste the copied HTML in the above editor, wait for a few seconds as the dynamic layout HTML is very large in size.<br />
+        • Click Parse to get parsed question, copy it to your clipboard and paste it in your CodeEditor.
+      </h3>
+    </>
+  );
 }
